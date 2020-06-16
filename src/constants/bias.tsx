923568fc3460cb0c31f7d6545c525e633397b803
aidenywl <yewwoei.low@u.nus.edu>
2020-06-16 10:08:17 +0800

    add bias info

--- src/constants/bias.tsx
+++ src/constants/bias.tsx
@@ -1,9 +1,28 @@
-
-const BIASES = [
-    {
-        name: "Demographic Bias",
-        description: "Text with presuppositions about particular genders, races, or other demographic categories.",
-        biased: "books are to mankind what memory is to the individual”,
-        unbiased: "books are to humankind what memory is to the individual”,
-    }
-]
+interface BiasInfo {
+  readonly name: string;
+  readonly description: string;
+  readonly biased: string;
+  readonly unbiased: string;
+}
+const BIASES: BiasInfo[] = [
+  {
+    name: 'Demographic Bias',
+    description:
+      'Text with presuppositions about particular genders, races, or other demographic categories.',
+    biased: 'books are to mankind what memory is to the individual',
+    unbiased: 'books are to humankind what memory is to the individual',
+  },
+  {
+    name: 'Framing Bias',
+    description: 'Using Subjective Words or phrases linked with a particular point of view',
+    biased: 'books are to mankind what memory is to the individual',
+    unbiased: 'books are to humankind what memory is to the individual',
+  },
+  {
+    name: 'Demographic Bias',
+    description:
+      'Text with presuppositions about particular genders, races, or other demographic categories.',
+    biased: 'books are to mankind what memory is to the individual',
+    unbiased: 'books are to humankind what memory is to the individual',
+  },
+];
