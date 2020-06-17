ce8a23cd3f1ee7ed6dcf5a612600be0173ecefa0
aidenywl <yewwoei.low@u.nus.edu>
2020-06-17 23:10:06 +0800

    add text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,79 @@
+import classNames from 'classnames';
+import React, { FunctionComponent } from 'react';
+
+import styles from './styles.css';
+
+export interface Props {
+  children: string;
+  className?: string;
+  color?: 'darkBlue' | 'darkGrey' | 'lightGrey' | 'standardGrey';
+  decoration?: 'none';
+  inline?: boolean;
+  overflow?: 'normal' | 'breakWord';
+  singleline?: boolean;
+  size?: 'l' | 'm' | 's' | 'xs';
+  textAlign?: 'center' | 'left' | 'right';
+  weight?: 'regular' | 'semibold';
+}
+
+const Text: FunctionComponent<Props> = ({
+  children,
+  className,
+  color,
+  decoration,
+  inline,
+  overflow,
+  singleline,
+  size,
+  textAlign,
+  weight,
+}) => {
+  const getTextClassName = (): string => {
+    const commonClassNames = [
+      styles.text,
+      {
+        [styles.colorDarkBlue]: color === 'darkBlue',
+        [styles.colorDarkGrey]: color === 'darkGrey',
+        [styles.colorLightGrey]: color === 'lightGrey',
+        [styles.colorStandardGrey]: color === 'standardGrey',
+      },
+      {
+        [styles.decorationNone]: decoration === 'none',
+      },
+      {
+        [styles.overflowNormal]: overflow === 'normal',
+        [styles.overflowBreakWord]: overflow === 'breakWord',
+      },
+      {
+        [styles.multiline]: !singleline,
+        [styles.singleline]: singleline,
+      },
+      {
+        [styles.textAlignCenter]: textAlign === 'center',
+        [styles.textAlignLeft]: textAlign === 'left',
+        [styles.textAlignRight]: textAlign === 'right',
+      },
+      {
+        [styles.weightRegular]: weight === 'regular',
+        [styles.weightSemibold]: weight === 'semibold',
+      },
+    ];
+
+    return classNames(commonClassNames, className, {
+      [styles.sizeL]: size === 'l',
+      [styles.sizeM]: size === 'm',
+      [styles.sizeS]: size === 's',
+      [styles.sizeXS]: size === 'xs',
+    });
+  };
+
+  const textClassName = getTextClassName();
+
+  if (!inline) {
+    return <p className={textClassName}>{children}</p>;
+  } else {
+    return <span className={textClassName}>{children}</span>;
+  }
+};
+
+export default Text;
