a42ca3bcae2d47074a26df2e6e458ed0c35b0fb1
aidenywl <yewwoei.low@u.nus.edu>
2020-06-18 15:12:44 +0800

    add constants and padding

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,17 @@
 module.exports = {
-	darkBlue: '#2f4858'
+  /** Spacing (Multipliers of 4) */
+  spacing4: '4px',
+  spacing12: '12px',
+  spacing16: '16px',
+  spacing24: '24px',
+  spacing36: '36px',
+  spacing48: '48px',
+
+  /** Colors */
+  darkBlue90: 'rgba(50, 75, 81, 0.9)',
+  darkBlue20: 'rgba(50, 75, 81, 0.2)',
+  darkGrey: '#333333',
+  lightGrey: '#828282',
+  standardGrey: 'rgba(79, 79, 79, 0.75)',
+  white70: 'rgba(255, 255, 255, 0.7)',
 };
